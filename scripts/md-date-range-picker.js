ff69e2b3ca590de17bb29d81a307a33e6c3b96a2
Marco Rosa <marco@bizaway.com>
2019-03-11 12:26:54 +0100

    [Auto] - Compiled distribution files

--- scripts/md-date-range-picker.js
+++ scripts/md-date-range-picker.js
@@ -739,6 +739,7 @@
 
                     console.log('emntra');
                     console.log('emntra');
+                        console.log('emntra');
 
                     $scope.cancel = function () {
                         $mdDialog.cancel();
